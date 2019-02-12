05dccd48dd3407b3c55cfdeaf35254b3aff8b664
Quang Ngoc <khuatquangngoc98@gmail.com>
2019-02-12 11:33:08 +0700

    Update install_gcloud.sh

--- install_gcloud.sh
+++ install_gcloud.sh
@@ -1,5 +1,6 @@
-sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo dpkg -i cuda-repo-ubuntu1604-10-0-local-10.0.130-410.48_1.0-1_amd64
+sudo apt-key add /var/cuda-repo-10-0-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
 

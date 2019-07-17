1e71d84df2dfd917fa5561b73fc17f1b616257f2
Quang Ngoc <qn.khuat@gmail.com>
2019-07-17 13:14:31 +0700

    Update setupvim.sh

--- setupvim.sh
+++ setupvim.sh
@@ -13,6 +13,8 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 
 # install nerdtree
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+# install ctags
+git clone https://github.com/ludovicchabant/vim-gutentags.git ~/.vim/bundle/vim-gutentags
 
 # setup vimrc
 curl https://raw.githubusercontent.com/qnkhuat/scripts/master/.vimrc -o ~/.vimrc
@@ -23,9 +25,9 @@ platform='unknown'
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
    # ubuntu 
-   apt-get install silversearcher-ag
+   apt-get install silversearcher-ag ctags
 elif [[ "$unamestr" == 'Darwin' ]]; then
    # macosx
    
-   brew install the_silver_searcher
+   brew install the_silver_searcher ctags
 fi

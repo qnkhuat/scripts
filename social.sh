b360de67aecf0c4d91040bddc90327aa0c7f50d3
Earther <qn.khuat@gmail.com>
2021-01-16 21:16:16 +0700

    auto backup: Update lock to use in-bash variable

--- social.sh
+++ social.sh
@@ -2,11 +2,14 @@ if [ $EUID != 0 ]; then
 	sudo "$0" "$@"
 	exit $?
 fi
+text="0.0.0.0 facebook.com instagram.com youtube.com www.facebook.com www.instagram.com www.youtube.com"
 if [ $# -eq 1 ] 
 then
-	sed -i '' '/^0.0.0.0/s/^/#/g' "/etc/hosts" 
+	#sed -i '' '/^0.0.0.0/s/^/#/g' "/etc/hosts" 
+	sed -i '' -e "/$text/d" /etc/hosts
 	echo "unlocked"
 else
-	sed -i '' '/^#0.0.0.0/s/^#//g' "/etc/hosts" 
+	#sed -i '' '/^#0.0.0.0/s/^#//g' "/etc/hosts" 
+	echo $text >> /etc/hosts
 	echo "locked"
 fi

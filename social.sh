e6561fc8ddd129c7febd4d22c00dc3f10d9dd20d
Quang Ngoc <qn.khuat@gmail.com>
2021-01-17 15:47:37 +0700

    Update social.sh

--- social.sh
+++ social.sh
@@ -2,8 +2,8 @@
 # usage:
 # type: `social` => lock all sites listed in &site below
 # type: `social [whatever]` => unlock sites
-# To add a site in the list. Add [site].com and www.[site].com to the $site
-# To make this a short cut: add alias social="path to this script" in your bash.rc or zsh.rc
+# To add a site in the list: add `[site].com` and `www.[site].com` to the $site
+# To call `social` anywhere: add `alias social="absolute path to this script"` in your bash.rc or zsh.rc
 
 # ask for sudo since the /etc/hosts is system file
 if [ $EUID != 0 ]; then

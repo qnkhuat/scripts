c995de3c9b5e12d6db21c40b77abd12b6b767081
Earther <qn.khuat@gmail.com>
2021-01-17 15:45:17 +0700

    auto backup: add emojis and readme

--- social.sh
+++ social.sh
@@ -1,14 +1,20 @@
-# a simple script to block social media sites
+# A simple script to block social media sites
+# usage:
+# type: `social` => lock all sites listed in &site below
+# type: `social [whatever]` => unlock sites
+# To add a site in the list. Add [site].com and www.[site].com to the $site
+# To make this a short cut: add alias social="path to this script" in your bash.rc or zsh.rc
+
 if [ $EUID != 0 ]; then
 	sudo "$0" "$@"
 	exit $?
 fi
-text="0.0.0.0 facebook.com www.facebook.com instagram.com www.instagram.com youtube.com www.youtube.com twitter.com www.twitter.com reddit.com www.reddit.com tinder.com www.tinder.com"
-if [ $# -eq 1 ] 
+site="0.0.0.0 facebook.com www.facebook.com instagram.com www.instagram.com youtube.com www.youtube.com twitter.com www.twitter.com reddit.com www.reddit.com tinder.com www.tinder.com"
+if [ $# -eq 1 ] # no arguments => lock
 then
-	sed -i '' -e "/$(echo $text | cut -d' ' -f1 )/d" /etc/hosts
-	echo "unlocked. Be quick"
-else
-	echo $text >> /etc/hosts
-	echo "locked. I'm proud of you"
+	sed -i '' -e "/$(echo $site | cut -d' ' -f1 )/d" /etc/hosts
+	echo "unlocked. Be quick ಠ_ಠ"
+else # if there are arguments => unlcok
+	echo $site>> /etc/hosts
+	echo "locked. I'm proud of you  (ง'̀-'́)ง"
 fi

7dd6427bbe9b5cf0e6863b77f56bc825ca355590
Earther <qn.khuat@gmail.com>
2021-01-17 13:04:41 +0700

    auto backup: add tinder to the lsit

--- social.sh
+++ social.sh
@@ -3,7 +3,7 @@ if [ $EUID != 0 ]; then
 	sudo "$0" "$@"
 	exit $?
 fi
-text="0.0.0.0 facebook.com www.facebook.com instagram.com www.instagram.com youtube.com www.youtube.com twitter.com www.twitter.com reddit.com www.reddit.com"
+text="0.0.0.0 facebook.com www.facebook.com instagram.com www.instagram.com youtube.com www.youtube.com twitter.com www.twitter.com reddit.com www.reddit.com tinder.com www.tinder.com"
 if [ $# -eq 1 ] 
 then
 	sed -i '' -e "/$text/d" /etc/hosts

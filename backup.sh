5953f4e91ee51a3c6b48e1f4e1bda457cea1c56a
Earther <qn.khuat@gmail.com>
2021-11-13 17:04:44 +0700

    auto backup:

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 cd ~/fun/scripts
-mkdir -p ._config
+mkdir -p ./_config
 cp -r ~/.config/nvim ./_config/nvim
 cp -r ~/.config/coc ./_config/coc
 cp ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json ~/.spacemacs ~/.clojure/deps.edn .

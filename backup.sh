9eb8890952418c123d655eaa2f88a973e8a87921
Earther <qn.khuat@gmail.com>
2021-11-15 09:09:00 +0700

    auto backup:

--- backup.sh
+++ backup.sh
@@ -2,7 +2,8 @@ cd ~/fun/scripts
 mkdir -p ./_config
 cp -r ~/.config/nvim ./_config/nvim
 cp -r ~/.config/coc ./_config/coc
-cp ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json ~/.spacemacs ~/.clojure/deps.edn ~/.emacs ~/.doom.d/ .
+cp -r ~/.doom.d ./_doom.d/
+cp ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json ~/.spacemacs ~/.clojure/deps.edn ~/.emacs .
 git add .
 git commit -m "auto backup: $1"
 git push origin master -f

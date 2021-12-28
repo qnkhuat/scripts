fc9cc4b2baa7b24f0139bd01b33a4b95d5eeee15
Ngoc Khuat <qn.khuat@gmail.com>
2021-12-28 14:27:53 +0700

    pull before backup

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 cd ~/fun/scripts
+git pull origin master
 mkdir -p ./_config
 cp -r ~/.config/nvim ./_config/nvim
 cp -r ~/.config/coc ./_config/coc
@@ -6,4 +7,4 @@ cp -r ~/.doom.d ./_doom.d/
 cp ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json ~/.spacemacs ~/.clojure/deps.edn ~/.emacs .
 git add .
 git commit -m "auto backup: $1"
-git push origin master -f
+git push origin master

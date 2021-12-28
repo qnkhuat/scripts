23505afafe515a7d98a5bc29a74da7d7366f9dcf
Ngoc Khuat <qn.khuat@gmail.com>
2021-12-28 14:29:05 +0700

    no more doom

--- backup.sh
+++ backup.sh
@@ -3,7 +3,6 @@ git pull origin master
 mkdir -p ./_config
 cp -r ~/.config/nvim ./_config/nvim
 cp -r ~/.config/coc ./_config/coc
-cp -r ~/.doom.d ./_doom.d/
 cp ~/.zshrc ~/.tmux.conf ~/.vimrc ~/.config/nvim/init.vim ~/.config/nvim/coc-settings.json ~/.spacemacs ~/.clojure/deps.edn ~/.emacs .
 git add .
 git commit -m "auto backup: $1"
